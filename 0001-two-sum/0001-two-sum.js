0abfa118a2d379efcd68e0c7b080b96ffb141833
andrew kim <76914884+k-rewd@users.noreply.github.com>
2023-04-26 01:23:52 -0700

    Time: 145 ms (22.55%), Space: 42.1 MB (86.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,14 +4,15 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let index = []
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
+    let res = []
+    for (let i = 0; i < nums.length; i ++) {
+        for (let j = i + 1; j < nums.length; j ++) {
             if (nums[i] + nums[j] === target) {
-                index.push(i);
-                index.push(j);
+                res.push(i)
+                res.push(j)
             }
         }
+        
     }
-    return index
+    return res
 };
